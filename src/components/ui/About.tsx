9f0052cbf5677fe404fb3cf66dbe898b9610fe7b
NaomiKleinhans <naomikleinhans56@gmail.com>
2024-05-13 21:10:53 +0200

    progress

--- src/components/ui/About.tsx
+++ src/components/ui/About.tsx
@@ -20,33 +20,22 @@ const About = () => {
 						About Me
 					</h2>
 					<div className='text-center text-textColor lg:text-xl md:text-lg sm:text-sm mx-4'>
-						Greetings! I am a front-end developer passionate about crafting
-						immersive digital experiences using cutting-edge technologies like
-						HTML, CSS, and JavaScript, particularly within the Next.js
-						framework. With Next.js, I specialize in transforming design
-						concepts into fully functional websites and applications that
-						captivate and inspire users. Collaboration is at the core of my
-						process. I meticulously follow designs created by designers,
-						meticulously translating them into seamless front-end experiences.
-						By closely adhering to design principles, I ensure that the final
-						product aligns perfectly with the envisioned user experience.
-						Continual learning and staying abreast of industry trends are
-						fundamental to my approach. This commitment ensures that I deliver
-						forward-thinking solutions that push the boundaries of what is
-						possible in front-end development. Thank you for exploring my
-						portfolio! I am thrilled about the opportunity to collaborate and
-						bring innovative ideas to life. Let us collaborate and create
-						something remarkable together! Beyond my front-end development
-						expertise, I hold a Bachelor of Science in Botany and Plant
-						Pathology (2020) and an Honours degree in Plant Health Ecology
-						(2021). With a year of internship experience in software
-						development, I am dedicated to contributing novel solutions in this
-						ever-evolving landscape, leveraging my unique blend of biological
-						expertise.
+						Hello! I'm a front-end developer passionate about creating immersive
+						digital experiences using HTML, CSS, and JavaScript, particularly
+						with Next.js. I specialize in turning design concepts into
+						functional websites and apps that captivate users. I closely follow
+						design principles to ensure a seamless user experience. Continual
+						learning and staying updated on industry trends are crucial to my
+						approach. Thank you for checking out my portfolio! Let's collaborate
+						and bring innovative ideas to life! Besides front-end development, I
+						have a Bachelor of Science in Botany and Plant Pathology (2020) and
+						an Honours degree in Plant Health Ecology (2021). With a year of
+						internship experience in software development, I'm dedicated to
+						contributing unique solutions in this ever-evolving field.
 					</div>
 				</div>
 			</div>
-		<CVPage />
+			<CVPage />
 			<div className='mt-1 text-md text-themeColorMain text-center'>
 				Feel free to download my CV!
 			</div>
